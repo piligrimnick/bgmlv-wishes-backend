943ab67e5760db421f57343745a9beeb6a92b4c9
Nikita Bogomolov <nbogomolov@taxdome.com>
2026-01-17 03:31:34 +0100

    Update docker config

--- app/jobs/daily_task_job.rb
+++ app/jobs/daily_task_job.rb
@@ -2,10 +2,11 @@ class DailyTaskJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Rails.logger.info "Starting DailyTaskJob at #{Time.current}"
-    
     result = Users::DeleteInactiveService.call
-    
-    Rails.logger.info "DailyTaskJob finished at #{Time.current}. Deleted #{result[:deleted_count]} users."
+    if result.success?
+      Rails.logger.info("Deleted #{result.value!} users.")
+    else
+      Rails.logger.error(result.failure)
+    end
   end
 end
